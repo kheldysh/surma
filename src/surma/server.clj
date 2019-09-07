085d2c89f09a65493d1019e400370df474ee2e81
Jesse Lankila <jesse.lankila@kodan.fi>
2019-09-07 16:09:13 +0300

    Add actual code in hope to run something

---
+++ src/surma/server.clj
@@ -0,0 +1,10 @@
+(ns surma.server
+  (:require [ring.adapter.jetty :as jetty]
+            [compojure-intro.handler :as handler])
+  (:gen-class))
+
+(defn -main
+  [& [port]]
+  (let [port (Integer. (or port (System/getenv "PORT") 3000))]
+    (jetty/run-jetty #'handler/app {:port  port
+                                    :join? false})))
