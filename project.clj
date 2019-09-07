04ba4ed50116cc7f773bd32c7ae6c015499a388f
Jesse Lankila <jesse.lankila@kodan.fi>
2019-09-07 15:55:55 +0300

    use lein 2.8.1

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [metosin/compojure-api "2.0.0-alpha30"]]
   :ring {:handler surma.handler/app}
   :uberjar-name "server.jar"
+  :min-lein-version "2.8.1"
   :profiles {:uberjar {:aot :all}
              :dev     {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
                        :plugins      [[lein-ring "0.12.5"]]}})
