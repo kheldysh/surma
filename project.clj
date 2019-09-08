f3595cc8a2b4e0f9a034ffb52347da6cb776770f
Jesse Lankila <jesse.lankila@kodan.fi>
2019-09-08 14:08:40 +0300

    remove unnecessary hooks

--- project.clj
+++ project.clj
@@ -8,7 +8,6 @@
   :uberjar-name "server.jar"
   :min-lein-version "2.8.1"
   :plugins [[lein-environ "1.1.0"]]
-  :hooks [environ.leiningen.hooks]
   :profiles {:uberjar {:aot :all}
              :dev     {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
                        :plugins      [[lein-ring "0.12.5"]]}})
